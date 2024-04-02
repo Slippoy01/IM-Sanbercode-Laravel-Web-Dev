bb19fa4b1292f819889537cef7d411662f914040
Slippoy01 <prakosoemir22@gmail.com>
2024-04-03 04:05:41 +0700

    commit ketiga: menambahkan file tugas ke 7, emir prakoso

---
+++ tugas-6/php-1/array.php
@@ -0,0 +1,76 @@
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array</title>
+</head>
+
+<body>
+    <h1>Berlatih Array</h1>
+
+    <?php
+    echo "<h3> Soal 1 </h3>";
+    // SOAL NO 1
+    $kids = ["Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"];
+    $adults = ["Hopper", "Nancy", "Joyce", "Jonathan", "Murray"];
+    echo "Ready";
+
+    echo "<h3> Soal 2</h3>";
+    // SOAL NO 2
+    echo "Cast Stranger Things: ";
+    echo "<br>";
+    echo "Total Kids: " . count($kids);
+    echo "<br><ol>";
+    foreach ($kids as $kid) {
+        echo "<li> $kid </li>";
+    }
+    echo "</ol>";
+
+    echo "Total Adults: " . count($adults);
+    echo "<br><ol>";
+    foreach ($adults as $adult) {
+        echo "<li> $adult </li>";
+    }
+    echo "</ol>";
+
+    echo "<h3>Soal No 3 </h3>";
+    // SOAL No 3
+    $characters = [
+        [
+            "Name" => "Will Byers",
+            "Age" => 12,
+            "Aliases" => "Will the Wise",
+            "Status" => "Alive"
+        ],
+        [
+            "Name" => "Mike Wheeler",
+            "Age" => 12,
+            "Aliases" => "Dungeon Master",
+            "Status" => "Alive"
+        ],
+        [
+            "Name" => "Jim Hopper",
+            "Age" => 43,
+            "Aliases" => "Chief Hopper",
+            "Status" => "Deceased"
+        ],
+        [
+            "Name" => "Eleven",
+            "Age" => 12,
+            "Aliases" => "El",
+            "Status" => "Alive"
+        ]
+    ];
+
+    echo "<pre>";
+    print_r($characters);
+    echo "</pre>";
+    ?>
+</body>
+
+</html>
