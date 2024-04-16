4c16df9f2278224e7ccdf17432507dea71f4fbfd
Slippoy01 <prakosoemir22@gmail.com>
2024-04-17 03:02:16 +0700

    commit kedelapan: menambahkan file tugas ke 12, emir prakoso

---
+++ tugas-6/tugas_12_laravel_opening/laravel_sanbercode/resources/views/page/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Welcome</title>
+</head>
+<body>
+
+<div>
+    <h1>SELAMAT DATANG {{$namaDepan}} {{$namaBlkg}}!</h1>
+    <p>Terima kasih telah bergabung di Sanberbook. Social Media kita bersama!</p>
+</div>
+
+</body>
+</html>
