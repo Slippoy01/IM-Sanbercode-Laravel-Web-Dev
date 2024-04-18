027a470a29ce221a77c7e38e294e63c1f07d21d9
Slippoy01 <prakosoemir22@gmail.com>
2024-04-19 01:45:18 +0700

    commit ke 10, menyelesaikan tugas ke 14, emir prakoso

---
+++ tugas-6/tugas_13/database/migrations/2024_04_18_183236_create_role_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('movie_id');
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->unsignedBigInteger('cast_id');
+            $table->foreign('cast_id')->references('id')->on('cast');
+            $table->string('name', 45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role');
+    }
+};
---
+++ tugas-6/tugas_13/database/migrations/2024_04_18_183743_create_critics_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('critics', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('movies_id');
+            $table->foreign('movies_id')->references('id')->on('movies');
+            $table->text('content');
+            $table->integer('point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('critics');
+    }
+};
